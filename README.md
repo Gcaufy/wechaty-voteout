3b6b338752a24210273a4764171700a915cb4138
Gcaufy <gcaufy@gmail.com>
2020-05-01 21:14:52 +0800

    fix: make it works

--- README.md
+++ README.md
@@ -41,10 +41,12 @@ const DEFAULT_CONFIG = {
   // When the people reach the target, then means (s)he has been voted out.
   target: 3,
   // Warn template, set to falsy to disable the warn message
-  warn: '你好，可能是因为你的聊天内容不当导致被用户投票，当累计票数达到 {{ target }} 时，你将被请出些群。你的当前票数为 {{ voted }}',
+  warnTemplate: '可能是因为你的聊天内容不当导致被用户投票，当前票数为 {{ count }}，当天累计票数达到 {{ target }} 时，你将被请出此群。',
+  // Kickout template, set to falsy to disable the message.
+  kickoutTemplate: '经 {{ voters }} 几人投票，你即将离开此群。',
   // Different puppet get different sign
   // We run more cases to see what sign it is, and update the comment here.
-  sign: ['弱', '<img class="qqemoji qqemoji80" text="[弱]_web" src="/zh_CN/htmledition/v2/images/spacer.gif" />'];
+  sign: ['[弱]', '[ThumbsDown]', '<img class="qqemoji qqemoji80" text="[弱]_web" src="/zh_CN/htmledition/v2/images/spacer.gif" />'],
 
   // The function to check if some one is voted.
   // Default function is to check is there a sign in the text.
@@ -53,6 +55,8 @@ const DEFAULT_CONFIG = {
   // E.g. ['Room1', 'Room2']
   // Set to falsy value means works for all rooms.
   room: false,
+  // Who never be kickedout by voting
+  whiteList: [],
   // Vote expred time, default to 1 day.
   expired: 24 * 3600 * 1000, // 1 day
 }
--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const DEFAULT_CONFIG = {
   // When the people reach the target, then means (s)he has been voted out.
   target: 3,
   // Warn template, set to falsy to disable the warn message
-  warn: '你好，可能是因为你的聊天内容不当导致被用户投票，当累计票数达到 {{ target }} 时，你将被请出些群。你的当前票数为 {{ voted }}',
+  warnTemplate: '可能是因为你的聊天内容不当导致被用户投票，当前票数为 {{ count }}，当天累计票数达到 {{ target }} 时，你将被请出此群。',
+  // Kickout template, set to falsy to disable the message.
+  kickoutTemplate: '经 {{ voters }} 几人投票，你即将离开此群。',
   // Different puppet get different sign
   // We run more cases to see what sign it is, and update the comment here.
-  sign: ['弱', '<img class="qqemoji qqemoji80" text="[弱]_web" src="/zh_CN/htmledition/v2/images/spacer.gif" />'];
+  sign: ['[弱]', '[ThumbsDown]', '<img class="qqemoji qqemoji80" text="[弱]_web" src="/zh_CN/htmledition/v2/images/spacer.gif" />'],
 
   // The function to check if some one is voted.
   // Default function is to check is there a sign in the text.
@@ -17,6 +19,8 @@ const DEFAULT_CONFIG = {
   // E.g. ['Room1', 'Room2']
   // Set to falsy value means works for all rooms.
   room: false,
+  // Who never be kickedout by voting
+  whiteList: [],
   // Vote expred time, default to 1 day.
   expired: 24 * 3600 * 1000, // 1 day
 }
@@ -33,7 +37,7 @@ module.exports = function WechatyVoteOutPlugin (config = {}) {
     config.sign = [ config.sign ];
   }
 
-  const cache = LRU({
+  const cache = new LRU({
     maxAge: config.expired,
     max: 1000, // hard code the max length of cache, 1000 is enough for the vote out case
   });
@@ -61,13 +65,13 @@ module.exports = function WechatyVoteOutPlugin (config = {}) {
 
       // According to the doc: https://wechaty.github.io/wechaty/#Message+mentionList
       // It only works in few puppet
-      let mentionList = await message.mentionList();
+      let mentionList = await m.mentionList();
 
       mentionList = mentionList.filter(contact => {
         return !config.whiteList.includes(contact.name()) && // (s)he is not in the white list
           !contact.self() && // (s)he is not the bot himself
           // TODO: I'm not sure can I check the room owner like this.
-          contact !== room.owner(); // (s)he is not the room owner.
+          contact.id !== room.owner().id; // (s)he is not the room owner.
       });
 
       // on one is mentioned
@@ -92,45 +96,67 @@ module.exports = function WechatyVoteOutPlugin (config = {}) {
       let votedOutList = [];
       let warnList = [];
 
-      mentionList.forEach((contact) => {
-        const cacheKey = topic + ':' + contact.name();
-        const voted = cache.get(cacheKey) || 0;
+      const voteBy = m.from();
 
-        // When reach the target. then kick out.
-        if (voted >= config.target - 1) {
-          votedOutList.push({
-            concat,
-            // Only delete the cache when the use was kickedout from the room
-            cb () { cache.del(cacheKey); }
-          });
-        } else {
-          voted++;
-          cache.set(cacheKey, voted);
-          if (config.warn) {
-            warnList.push({
+      mentionList.forEach((contact) => {
+        const cacheKey = room.id + ':' + contact.id;
+        const voted = cache.get(cacheKey) || { count: 0, voters: [] };
+
+        const isdup = voted.voters.find(voter => voter.id === voteBy.id);
+        if (voted.count >= config.target || !isdup) {
+          voted.count++;
+          if (!isdup) {
+            voted.voters.push(voteBy);
+            cache.set(cacheKey, voted);
+          }
+          // When reach the target. then kick out.
+          if (voted.count >= config.target) {
+            votedOutList.push({
               contact,
-              voted,
+              voters: voted.voters,
+              // Only delete the cache when the use was kickedout from the room, so that peopel can vote again
+              cb () {
+                cache.del(cacheKey);
+              }
             });
+          } else {
+            if (config.warnTemplate) {
+              warnList.push({
+                contact,
+                voted,
+              });
+            }
           }
         }
       });
 
-      const actions = [];
+      let actions = [];
       if (warnList.length) {
         actions = actions.concat(warnList.map(item => {
-          return room.say(Mustache.render(config.warn, {
+          return room.say(mustache.render(config.warnTemplate, {
             name: item.contact.name(),
-            voter: m.contact().name(),
-            room: room.name(),
+            voter: voteBy.name(),
+            room: room.topic(),
             voted: item.voted,
+            count: item.voted.count,
             target: config.target,
-          }), item.concat);
+          }), item.contact);
         }));
       }
 
       if (votedOutList.length) {
         actions = actions.concat(votedOutList.map(item => {
-          return room.del(item.concat).then(() => item.cb());
+          if (config.kickoutTemplate) {
+            return room.say(mustache.render(config.kickoutTemplate, {
+              name: item.contact.name(),
+              voters: item.voters.map(voter => voter.name()).join(',')
+            }), item.contact).catch(e => {
+              // ignore say error
+            }).then(() => {
+              return room.del(item.contact).then(() => item.cb());
+            });
+          }
+          return room.del(item.contact).then(() => item.cb());
         }));
       }
       if (actions.length) {
