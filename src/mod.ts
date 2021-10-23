322e5911f03a4f39319d92f65be58a1de9b69506
Huan (李卓桓) <zixia@zixia.net>
2021-10-23 20:38:11 +0800

    v1.0 with ESM

---
+++ src/mod.ts
@@ -0,0 +1,9 @@
+export {
+  VoteOut,
+}                 from './vote-out.js'
+export type {
+  VoteOutConfig,
+}                 from './config.js'
+export {
+  VERSION,
+}                 from './version.js'
